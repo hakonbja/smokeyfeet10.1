924728b58faa8740f006dc5a34c5fd5d2b710332
Hákon <hakonbja@gmail.com>
2019-12-07 01:24:52 +0100

    add artists page transition

--- src/components/Artists/index.js
+++ src/components/Artists/index.js
@@ -19,7 +19,7 @@ const Artists = (props) => {
 
   return (
     <div className={styles.artists}>
-      <Redirect to="/artists/alice-felipe" />
+      {/* <Redirect to="/artists/alice-felipe" /> */}
       {ArtistsMenu}
       <Route path="/artists/:slug">
         <ArtistInfo artists={artists.info} isDesktop={props.isDesktop}/>
